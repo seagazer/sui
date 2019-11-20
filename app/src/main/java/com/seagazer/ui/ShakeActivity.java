2f93f172c5128fe246cb4b623aef6beb027af80b
seagazer <seagazer@126.com>
2019-11-20 19:01:09 +0800

    增加边界自动开启抖动动效的布局控件(类似爱奇艺效果)

---
+++ app/src/main/java/com/seagazer/ui/ShakeActivity.java
@@ -0,0 +1,14 @@
+package com.seagazer.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShakeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shake);
+    }
+}
